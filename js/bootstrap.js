01fe2b975a38da04d09274ba3f7e550afcfee60a
chrisirhc <chris.sirhc@gmail.com>
2010-04-08 11:36:38 +0800

    Fixed some bugs. Psession is in rudimentary work condition.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -72,15 +72,20 @@ GET(/\/class\/?/, function () {
     }
 });
 
-/** Contain the user ID and the name to display as well as photograph to display */
+/**
+ *  Contain the user ID and the name to display as well as photograph to display
+ */
 var Profile = new Resource("profile",
-{
-    '@constructor': function (userId, name, photograph) {
-        this.userId = userId;
-        this.name = name;
-        this.photograph = photograph;
-    }
-});
+        /** @lends Profile */
+        {
+        /** @constructs */
+            '@constructor': function (userId, name, photograph) {
+                this.userId = userId;
+                this.name = name;
+                this.photograph = photograph;
+            }
+        }
+);
 Profile.transient = false;
 
 // Show new form
@@ -100,7 +105,7 @@ GET(/\/profile\/delete\/(.+)$/, function ( profileId ) {
 // Show edit form
 GET(/\/profile\/(.+)$/, function ( profileId ) {
     try {
-        this.aprofile = profile.get( profileId );
+        this.aprofile = Profile.get( profileId );
     } catch(e) {
         this.aprofile = { userId: null, name: null, photograph: null};
     }
@@ -144,41 +149,95 @@ var Psession = new Resource("psession",
     /*
      * TODO May not have the endTime during the creation of this object
      */
-    '@constructor': function(participantId, startTime, endTime) {
-
+    '@constructor': function(participantId, classId, startTime, endTime) {
+        this.participantId = participantId;
+        this.classId = classId;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
 );
 Psession.transient = false;
 
-GET(/\/psession\/?/, function () {
-    var psession = Psession.search({});
-    if (psession.length) {
-        this.psession = psession;
-        return template("psessionlist.html");
-    } else {
-        return "nothing here";
-    }
-});
 // Start psession
 GET(/\/psession\/start\/(.+)$/, function ( profileId ) {
     try {
-        this.aprofile = profile.get( profileId );
+        this.aprofile = Profile.get( profileId );
     } catch(e) {
-        this.aprofile = { userId: null, name: null, photograph: null};
+        this.aprofile = { id: profileId, userId: "anonymous (" + profileId + ")", 
+                name: null, photograph: null};
     }
-    Psession.search({participantId: this.aprofile.userId}, {limit: 1, sort: created});
+    // TODO get the class
+    
+    // only care about the last one
+    var psessionList = 
+        Psession.search({participantId: this.aprofile.id}, {sort: 'created'}); 
 
-    // Send out XML RPC method to the Push API to push to the Android App
+    if (psessionList.length && psessionList[psessionList.length-1].endTime == "") {
+        // shift forward the startTime
+        psessionList[psessionList.length-1].startTime = Date.now().toString();
+        psessionList[psessionList.length-1].save();
+    } else {
+        // create new psession
+        this.apsession = new Psession(this.aprofile.id, "Demo", Date.now().toString(), "");
+        this.apsession.save();
+    }
+    return JSON.stringify({ok: true});
+    // TODO Send out XML RPC method to the Push API to push to the Android App
+    /*
+    system.http.request("POST", "http://mp.labs.ericsson.net/mp/rpc", [],
+            <methodCall>
+              <methodName>getApplicationList</methodName>
+              <params>
+                <param>
+                    <value><string>3f73a881746726650649bba254ea00e0</string></value>
+                </param>
+              </params>
+            </methodCall>);
+    */
 });
 // Stop psession
 GET(/\/psession\/stop\/(.+)$/, function ( profileId ) {
     try {
-        this.aprofile = profile.get( profileId );
+        this.aprofile = Profile.get( profileId );
     } catch(e) {
-        this.aprofile = { userId: null, name: null, photograph: null};
+        this.aprofile = { id: profileId, userId: "anonymous (" + profileId + ")", 
+                name: null, photograph: null};
     }
-    return template("profileform.html");
+    var psessionList =
+        Psession.search({participantId: this.aprofile.id}, {sort: 'created'});
+    
+    // Only if there is something to stop
+    if (psessionList.length && psessionList[psessionList.length-1].endTime == "") {
+        // might want to use timestamp from the Android app instead
+        psessionList[psessionList.length-1].endTime = Date.now().toString();
+        psessionList[psessionList.length-1].save();
+    // Remember threshold
+    /*
+    if (Date.parse(first).add({seconds: 5}).isBefore(Date.parse(second)))
+    remove;
+     */
+    }
+    return JSON.stringify({ok: true});
+    // Send out XML RPC method to the Push API for the teacher's app
+});
+//Delete psession
+GET(/\/psession\/delete\/(.+)$/, function ( psessionId ) {
+    try {
+        Psession.remove(psessionId);
+    } catch(e) {
+    }
+    redirect("/psession");
+});
+
+GET(/\/psession\/?/, function () {
+    var psession = Psession.search({});
+    if (psession.length) {
+        this.psessionList = psession;
+        return template("psessionlist.html");
+    } else {
+        return "nothing here";
+    }
 });
 
 /*
