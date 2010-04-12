fea4a4f56b93b12ff784056ecbe6d110f09d8bc6
chrisirhc <chris.sirhc@gmail.com>
2010-04-12 18:07:22 +0800

    Added the check for profileId.

--- js/Register.js
+++ js/Register.js
@@ -20,26 +20,29 @@ GET(/\/getProfileId\/(.+)$/, function (userId) {
  */
 GET(/\/register\/(.+)$/, function (profileId) {
     var aprofile;
-    try {
-        aprofile = Profile.get(profileId);
-    } catch (e) {
-        return JSON.stringify({ok: false});
-    }
-    var classList = InClass.search({'profileId': profileId});
-    var aclass, i;
-    for (i in classList) {
+    if (profileId != "0") {
         try {
-            aclass = Pclass.get(classList[i].classId);
-            if (Date.now()
-                    .between( // in sg
-                            Date.parse(aclass.startTime).setTimezoneOffset(+0800),
-                            Date.parse(aclass.endTime).setTimezoneOffset(+0800))
-                            ) {
-                return JSON.stringify({ok: true, pclass: aclass});
+            aprofile = Profile.get(profileId);
+        } catch (e) {
+            return JSON.stringify({ok: false});
+        }
+
+        var classList = InClass.search({'profileId': profileId});
+        var aclass, i;
+        for (i in classList) {
+            try {
+                aclass = Pclass.get(classList[i].classId);
+                if (Date.now()
+                        .between( // in sg
+                                Date.parse(aclass.startTime).setTimezoneOffset(+0800),
+                                Date.parse(aclass.endTime).setTimezoneOffset(+0800))
+                ) {
+                    return JSON.stringify({ok: true, pclass: aclass});
+                }
+            } catch(e) {
+                return JSON.stringify(e);
+                // do nothing
             }
-        } catch(e) {
-            return JSON.stringify(e);
-            // do nothing
         }
     }
     return JSON.stringify({ok: true, pclass: {id: "0", classTitle: "DEMO Class"}});
