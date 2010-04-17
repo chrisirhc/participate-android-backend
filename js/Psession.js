769fe160f9a75d0f833a06a84aed60aba12bda39
chrisirhc <chris.sirhc@gmail.com>
2010-04-17 16:27:32 +0800

    + Moved some logic around in the Register.js and   created the getClassFromProfile method
    + Psession now uses the getClassFromProfile
      method
    
    + Added formatting information in the class form

--- js/Psession.js
+++ js/Psession.js
@@ -24,31 +24,31 @@ GET(/\/psession\/start\/(.+)$/, function (profileId) {
             photograph : null
         };
     }
-    // TODO get the class
+	// This is from the Register.js always returns a class, whether invalid ("DEMO class") or not
+	var pclass = getClassFromProfile(profileId);
 
-        // only care about the last one
-        var psessionList = Psession.search( {
-            participantId : this.aprofile.id
-        }, {
-            sort : 'created'
-        });
-
-        if (psessionList.length
-                && psessionList[psessionList.length - 1].endTime == "") {
-            // shift forward the startTime
-            psessionList[psessionList.length - 1].startTime = Date.now()
-                    .toString();
-            psessionList[psessionList.length - 1].save();
-        } else {
-            // create new psession
-            this.apsession = new Psession(this.aprofile.id, "Demo", Date.now()
-                    .toString(), "");
-            this.apsession.save();
-        }
-        return JSON.stringify( {
-            ok : true, 'psession': this.apsession
-        });
+    // only care about the last one
+    var psessionList = Psession.search( {
+        participantId : this.aprofile.id
+    }, {
+        sort : 'created'
     });
+
+    if (psessionList.length
+            && psessionList[psessionList.length - 1].endTime == "") {
+        // shift forward the startTime
+        psessionList[psessionList.length - 1].startTime = Date.now().getTime();
+        psessionList[psessionList.length - 1].save();
+		this.apsession = psessionList[psessionList.length - 1];
+    } else {
+        // create new psession
+        this.apsession = new Psession(this.aprofile.id, pclass.id, Date.now().getTime(), "");
+        this.apsession.save();
+    }
+    return JSON.stringify( {
+        ok : true, 'psession': this.apsession
+    });
+});
 // Stop psession
 GET(/\/psession\/stop\/(.+)$/, function (profileId) {
     try {
@@ -70,10 +70,12 @@ GET(/\/psession\/stop\/(.+)$/, function (profileId) {
     // Only if there is something to stop
         if (psessionList.length
                 && psessionList[psessionList.length - 1].endTime == "") {
-            // might want to use timestamp from the Android app instead
-        psessionList[psessionList.length - 1].endTime = Date.now().toString();
+            // TODO might want to use timestamp from the Android app instead
+        psessionList[psessionList.length - 1].endTime = Date.now().getTime();
         psessionList[psessionList.length - 1].save();
-        // Remember threshold
+
+		// TODO possibly add this as a security feature in the future
+        // enforce threshold
         /*
          * if (Date.parse(first).add({seconds: 5}).isBefore(Date.parse(second)))
          * remove;
--- js/Register.js
+++ js/Register.js
@@ -18,32 +18,71 @@ GET(/\/getProfileId\/(.+)$/, function (userId) {
 /**
  * The functionality for registering a user...
  */
-GET(/\/register\/(.+)$/, function (profileId) {
-    var aprofile;
+GET(/\/register\/(.+)$/, function(profileId){
+	return JSON.stringify({ok: true, pclass: getClassFromProfile(profileId)});
+});
+
+/**
+ * Always returns a class that the profileId is currently attending
+ * If there is no such class, it will return the "DEMO class"
+ * @param {Object} profileId
+ * @return {Object} pclass The current class that the student is attending
+ */
+function getClassFromProfile(profileId) {
     if (profileId != "0") {
+		// check whether such a profile exists
+	    var aprofile;
         try {
             aprofile = Profile.get(profileId);
         } catch (e) {
-            return JSON.stringify({ok: false});
+			// do nothing if there isn't since i'll check later
         }
 
-        var classList = InClass.search({'profileId': profileId});
-        var aclass, i;
-        for (i in classList) {
-            try {
-                aclass = Pclass.get(classList[i].classId);
-                if (Date.now()
-                        .between( // in sg
-                                Date.parse(aclass.startTime).setTimezoneOffset(+0800),
-                                Date.parse(aclass.endTime).setTimezoneOffset(+0800))
-                ) {
-                    return JSON.stringify({ok: true, pclass: aclass});
-                }
-            } catch(e) {
-                return JSON.stringify(e);
-                // do nothing
-            }
-        }
+		if (aprofile != null) {
+	        var classList = InClass.search({'profileId': profileId});
+	        var aclass, i;
+	        for (i in classList) {
+	            try {
+	                aclass = Pclass.get(classList[i].classId);
+	                if (Date.now()
+	                        .between( // in sg
+	                                Date.parse(aclass.startTime).setTimezoneOffset(+0800),
+	                                Date.parse(aclass.endTime).setTimezoneOffset(+0800))
+	                ) {
+	                    return aclass;
+	                }
+	            } catch(e) {
+					// even if he has a profile, throw him to the DEMO class
+	            }
+	        }
+		}
     }
-    return JSON.stringify({ok: true, pclass: {id: "0", classTitle: "DEMO Class"}});
+    return {id: "0", classTitle: "DEMO Class"};
+}
+/**
+ * Get all the psessions for the class, and all the
+ * ratings for each psession (the sum will do)
+ */
+GET(/\/pclasspart\/(.+)?/, function(classId) {
+	// get this class
+	var pclass = Pclass.get(classId);
+	// find all psessions under this class
+	// TODO test whether this really works (the created thing)
+	var ratingList;
+
+	// find all psessions in the current slot
+	var psessionList = Psession.search({'classId': classId,
+					'startTime': {'>=': new Date.parse(pclass.startTime).getTime()},
+					'endTime': {'<=' : new Date.parse(pclass.endTime).getTime()}});
+	if (psessionList.length) {
+		for (var i in psessionList) {
+			ratingList = Rating.search({'psessionId': psessionList[i].id});
+			psessionList[i].rating = 0;
+			for (var j in ratingList) {
+				psessionList[i].rating += ratingList[j].ratingPoints;
+			}
+		}
+	}
+	return JSON.stringify(psessionList);
+	// forget about efficiency
 });
