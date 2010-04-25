0166599f9557b28f3dae48c574f9aa11b8c5c5ca
chrisirhc <chris.sirhc@gmail.com>
2010-04-25 22:19:47 +0800

    Added an index page. Cleaned up bootstrap.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -18,63 +18,6 @@ system.use("InClass");
 // Take care of registering teacher / student
 system.use("Register");
 
-// Testing the XML-RPC into the Ericsson Push API
-GET("/push", function() {
-    // testing the XML-RPC
-    return JSON.stringify(system.http.request("POST", "http://mp.labs.ericsson.net/mp/rpc", [],
-    <methodCall>
-      <methodName>getApplicationList</methodName>
-      <params>
-        <param>
-            <value><string>3f73a881746726650649bba254ea00e0</string></value>
-        </param>
-      </params>
-    </methodCall>).headers);
-    // .content
-});
-
-
-GET("/pushout", function() {
-    // testing the XML-RPC
-    return JSON.stringify(system.http.request("POST", "http://mp.labs.ericsson.net/mp/rpc", [],
-    <methodCall>
-      <methodName>getApplicationList</methodName>
-      <params>
-        <param>
-            <value><string>3f73a881746726650649bba254ea00e0</string></value>
-        </param>
-      </params>
-    </methodCall>).headers);
-    // .content
-});
-
-// Profile functions
-// I think we might need to prepoulate this
-
-/*
- * function main(aRequest) { return "hi participate!"; }
- */
-
-// Ambient Display Retrieve
-GET(/\/user\/(.+)$/, function(userId) {
-    this.response.mime = 'application/json';
-    var json = {};
-    try {
-        // this.profile = Profile.get( userId );
-        /*
-         * find all the ratings under this user
-         */
-        var ratings = Rating.search({ratee: userId}, {limit: 10});
-        for (var x in ratings) {
-            json[x] = ratings[x];
-        }
-    } catch(e) {
-        return true;
-    }
-    return json;
-});
-
-// Rating a user
-PUT(/\/user\/(.+)$/, function(userId) {
-    this.response;
+GET("/", function() {
+  return template("index.html");
 });
